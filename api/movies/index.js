f03244675b439702820a41fdffdcf4c5abf48d23
zft666zft <2810458566@qq.com>
2024-12-18 18:53:28 +0000

    Added Pagination

--- api/movies/index.js
+++ api/movies/index.js
@@ -9,9 +9,26 @@ import {
 const router = express.Router();
 
 router.get('/', asyncHandler(async (req, res) => {
-    const movies = await movieModel.find();
-    res.status(200).json(movies);
+    let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
+    [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+
+    // Parallel execution of counting movies and getting movies using movieModel
+    const [total_results, results] = await Promise.all([
+        movieModel.estimatedDocumentCount(),
+        movieModel.find().limit(limit).skip((page - 1) * limit)
+    ]);
+    const total_pages = Math.ceil(total_results / limit); //Calculate total number of pages (= total No Docs/Number of docs per page) 
+
+    //construct return Object and insert into response object
+    const returnObject = {
+        page,
+        total_pages,
+        total_results,
+        results
+    };
+    res.status(200).json(returnObject);
 }));
+
 
 // Get movie details
 router.get('/:id', asyncHandler(async (req, res) => {
