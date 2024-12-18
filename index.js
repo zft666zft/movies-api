e3369764e02fbe848243dd2bfeb4ff2bbfc694f9
zft666zft <2810458566@qq.com>
2024-12-18 18:47:01 +0000

    Added Auth Middleware

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler'
 import moviesRouter from './api/movies';
+import authenticate from './authenticate';
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
-app.use('/api/movies', moviesRouter);
+app.use('/api/movies', authenticate, moviesRouter);
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
