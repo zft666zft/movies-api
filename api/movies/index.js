361bd94c4f6e17c1c0d7de174fdf1e145b4e35d4
zft666zft <2810458566@qq.com>
2024-12-18 16:59:35 +0000

    Added Movies Route

---
+++ api/movies/index.js
@@ -0,0 +1,23 @@
+import movieModel from './movieModel';
+import asyncHandler from 'express-async-handler';
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', asyncHandler(async (req, res) => {
+    const movies = await movieModel.find();
+    res.status(200).json(movies);
+}));
+
+// Get movie details
+router.get('/:id', asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id);
+    const movie = await movieModel.findByMovieDBId(id);
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
+    }
+}));
+
+export default router;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
 import defaultErrHandler from './errHandler'
+import moviesRouter from './api/movies';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
+app.use('/api/movies', moviesRouter);
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
